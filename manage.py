bba5d30ddab7a31b706d0c2b5069e137f52db243
Edmar Junyor Bevilaqua <edmar.bevi@gmail.com>
2024-05-20 14:19:00 -0300

    Update manage.py
    The manage.py runserver wasn't working because it was trying to access the old "RSF" folder.

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "RSF.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "J&J.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
