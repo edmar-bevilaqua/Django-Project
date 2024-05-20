91641baa787dcf9d21abfe00714311883e08ab5c
Edmar Junyor Bevilaqua <edmar.bevi@gmail.com>
2024-05-20 14:17:18 -0300

    Update folder name
    Updated the folder name from "RSF" to "J&J" and changed all the setting that was pointing to /RSF

--- J&J/asgi.py
+++ J&J/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for RSF project.
+ASGI config for J&J project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "RSF.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "J&J.settings")
 
 application = get_asgi_application()
--- J&J/wsgi.py
+++ J&J/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for RSF project.
+WSGI config for J&J project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "RSF.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "J&J.settings")
 
 application = get_wsgi_application()
