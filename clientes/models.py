a48db3bf084282e7f1b22eb4f22aa6051d29df44
Edmar Junyor Bevilaqua <edmar.bevi@gmail.com>
2024-05-21 17:25:58 -0300

    Changed virtual enviroment

--- clientes/models.py
+++ clientes/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=50)
+    sobrenome = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    cpf = models.CharField(max_length=12)
+    
+    def __str__(self) -> str:
+        return self.nome   
+
+class Pet(models.Model):
+    nome_pet = models.CharField(max_length=50)
+    data_nascimento_pet = models.DateField()
+    porte = models.CharField(max_length=7)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    banhos = models.IntegerField(default=0)
+    tosas = models.IntegerField(default=0)
