7c6421b65d83856e620c028491fc1950877a99b6
Edmar Junyor Bevilaqua <edmar.bevi@gmail.com>
2024-06-02 02:12:23 -0300

    Apply authentication in every app and page
    The system is locked, only authenticated users can add, view and edit the content of clients and services.

--- users/views.py
+++ users/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
+
+def login_user(request):
+    if request.method == "GET":
+        return render(request, 'authenticate/login.html', {})
+    elif request.method =="POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index.html')
+        else:
+            messages.success(request, ("There were a mistake, try again!"))
+            return redirect('login_user')
+        
+def logout_user(request):
+    if request.method == "GET":
+        logout(request)
+        messages.success(request, ("You have succesfully logged out!"))
+        return redirect('index.html')
